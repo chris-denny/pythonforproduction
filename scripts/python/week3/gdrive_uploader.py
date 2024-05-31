94d6c072c4f8254aca7bc156f025954ae726cc41
Chris Denny <40133077+chrisdenny1@users.noreply.github.com>
2024-05-31 17:10:27 +0100

    Removed unecessary timeout

--- scripts/python/week3/gdrive_uploader.py
+++ scripts/python/week3/gdrive_uploader.py
@@ -1,18 +1,13 @@
 import os
-import time
 import sys
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
 def upload_file(file_path, file_name):
-    # Wait for the file to exist with a timeout of 10s
-    timeout = 10
-    start_time = time.time()
-    while not os.path.exists(file_path):
-        if time.time() - start_time > timeout:
-            print(f"Timeout: File {file_name} did not exist after {timeout} seconds.")
-            sys.exit()
-        time.sleep(0.1)  # Check every 100ms
+    # Check if the file exists.
+    if not os.path.exists(file_path):
+        print(f"{file_name} does not exist.")
+        sys.exit()
 
     # Create a GoogleAuth instance with client secrets file.
     gauth = GoogleAuth(settings_file=os.path.join(os.path.dirname(__file__), "settings.yaml"))
@@ -27,5 +22,4 @@ def upload_file(file_path, file_name):
     print(f"Uploading {file_path} to Google Drive...")
     file1.Upload()
     print(f"Uploaded title: {file1['title']}, id: {file1['id']}")
-# Usage Example
-# upload_file(os.path.join(os.getcwd(), "hello.txt"), "hello.txt")
+
